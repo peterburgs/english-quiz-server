73305a1fee1a24861113ad13be1551cb46c3aef3
PeterBurgs <peterburgs.vn@gmail.com>
2020-11-25 15:43:50 +0700

    connect to mongoDB successfully

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const app = require("./index");
+const server = http.createServer(app);
+
+console.log("*LOG: Connecting to server");
+
+// Use port 3001
+const port = process.env.PORT || 3001;
+
+server.listen(port);
+if (!server) {
+  console.log("*LOG: Could not start server on port " + port);
+} else {
+  console.log(
+    "*LOG: Connected successfully! \nServer listening on port " + port
+  );
+}
