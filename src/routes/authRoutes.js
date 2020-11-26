18f3fac925f2fdf8f8519663b05ff6a763b275a8
PeterBurgs <peterburgs.vn@gmail.com>
2020-11-26 21:13:29 +0700

    finish signin route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
@@ -46,6 +45,43 @@ router.post("/signup", async (req, res) => {
     });
 });
 
-// Export
+// GET method: Sign In
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
 
+  // Validate empty input
+  if (!email || !password) {
+    return res.status(422).json({
+      error: "Email or Password is incorrect",
+    });
+  }
+
+  const userCredential = await UserCredential.findOne({ email });
+
+  // Validate email is existed or not
+  if (!userCredential) {
+    return res.status(404).json({
+      error: "Email not found",
+    });
+  }
+
+  // Compare registered email and providing email
+  try {
+    await userCredential.comparePassword(password);
+
+    const token = jwt.sign(
+      { userCredentialId: userCredential._id },
+      String(process.env.SECRET_KEY)
+    );
+    res.status(200).json({
+      message: "Log in successful",
+      token,
+    });
+  } catch (err) {
+    return res.status(422).json({
+      error: "Invalid email or password",
+    });
+  }
+});
+// Export
 module.exports = router;
